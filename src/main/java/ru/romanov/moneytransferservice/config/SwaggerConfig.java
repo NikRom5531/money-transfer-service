c726198305caf886f068f2ae88ab6f1a96eeb029
Romanov N.A. <kolya-2012@mail.ru>
2024-06-20 22:10:48 +0300

    Добавлена конфигурация Swagger для документирования API.

---
+++ src/main/java/ru/romanov/moneytransferservice/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package ru.romanov.moneytransferservice.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Конфигурация Swagger для документирования API.
+ */
+@Configuration
+public class SwaggerConfig {
+
+    /**
+     * Создает и настраивает экземпляр GroupedOpenApi для публичного API.
+     *
+     * @return настроенный GroupedOpenApi
+     */
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
+
